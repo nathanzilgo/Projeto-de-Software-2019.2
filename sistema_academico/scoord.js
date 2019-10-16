d8afccaacc1bf329c333079bb98258e478443519
nathan <nathanfpedroza@gmail.com>
2019-10-16 17:16:18 -0300

    up

---
+++ sistema_academico/scoord.js
@@ -0,0 +1,12 @@
+function disciplina(id, nome, creditos, pre_req) {
+    let d = {
+        id: id,
+        nome: nome,
+        creditos: creditos,
+        pre_requisitos: pre_req,
+    }
+    d.getNome() = () {
+        return d.nome;
+    }
+
+}
