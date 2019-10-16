336813aea532829d92d018ff4e2058671af860af
nathan <nathanfpedroza@gmail.com>
2019-10-16 17:37:16 -0300

    disciplina passando nos teste

--- sistema_academico/scoord.js
+++ sistema_academico/scoord.js
@@ -1,25 +1,21 @@
 function disciplina(id, nome, creditos, pre_req) {
+
     let d = {
         id: id,
         nome: nome,
         creditos: creditos,
         pre_requisitos: pre_req,
-    }
-
-    d.get_nome = () => {
-        d.nome;
-    }
-
-    d.id = () => {
-        d.id;
-    }
-
-    d.creditos = () => {
-        d.creditos;
-    }
-
-    d.set_nome = function (newNome) {
-        d.nome = newNome;
-    }
-
+        get_nome: function () {
+            return nome;
+        },
+        id: function () {
+            return id;
+        },
+        set_nome: function (newNome) {
+            nome = newNome
+        },
+    };
+    return d;
 }
+
+exports.disciplina = disciplina;
