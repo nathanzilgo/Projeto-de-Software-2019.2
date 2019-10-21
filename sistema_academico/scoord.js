9b1b16ba8a6e5d170d253c2c49771de9e4351554
nathan <nathanfpedroza@gmail.com>
2019-10-21 01:21:37 -0300

    continuando o lab

--- sistema_academico/scoord.js
+++ sistema_academico/scoord.js
@@ -1,3 +1,5 @@
+possiveisEstados = ["planejada", "ativa", "concluída"];
+
 function disciplina(id, nome, creditos, pre_req) {
 
     let d = {
@@ -5,17 +7,81 @@ function disciplina(id, nome, creditos, pre_req) {
         nome: nome,
         creditos: creditos,
         pre_requisitos: pre_req,
+
         get_nome: function () {
             return nome;
         },
+
         id: function () {
             return id;
         },
+
         set_nome: function (newNome) {
             nome = newNome
         },
-    };
+    }
+
     return d;
+}
+
+function turma(disc, periodo) {
+
+    let t = {
+        disciplina: disc,
+        periodo: periodo,
+        professor: null,
+        estudantes: [],
+
+        setProfessor: function (nome) {
+            this.professor = nome;
+        },
+
+        matriculaEstudantes: function (estudante) {
+            if (!(estudante in this.estudantes) && status != possiveisEstados[2]) {
+                this.estudantes.push(estudante);
+            }
+        },
+
+        status: possiveisEstados[0],
+
+        set_status: function (novo) {
+            if (novo in possiveisEstados) {
+                this.status = novo;
+            }
+        }
+    }
+
+    return t;
+}
+
+function professor(matricula, nome, email, cpf, foto) {
+    let privateMatricula = matricula;
+
+    let p = {
+        nome: nome,
+        email: email,
+        cpf: cpf,
+        foto: foto,
+        turmas: [],
+
+        get_Matricula: () => {
+            return privateMatricula
+        },
+
+        set_Nome: function (novo) {
+            this.nome = novo;
+        },
+
+        aloca_turma: function (t) {
+            t.professor = this;
+        },
+
+        turmas(semestre) {
+
+        }
+    }
+
+    return p;
 }
 
 exports.disciplina = disciplina;
